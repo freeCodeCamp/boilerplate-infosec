f65424659f5e615d09f46f468735de7c60b7fa4e
Mateus <creztfallen@M.S.M>
2022-09-27 13:18:22 -0300

    1st challenge

--- myApp.js
+++ myApp.js
@@ -1,58 +1,13 @@
 const express = require('express');
 const app = express();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const helmet = require('helmet');
 
 module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
-app.get("/", function (request, response) {
+app.get('/', function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 let port = process.env.PORT || 3000;
