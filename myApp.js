4ccd1f86ae779041390c464a9830f8d6119982b4
Mateus <creztfallen@M.S.M>
2022-09-27 13:32:44 -0300

    helmet frameguard

--- myApp.js
+++ myApp.js
@@ -4,6 +4,8 @@ const helmet = require('helmet');
 
 module.exports = app;
 const api = require('./server.js');
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({ action: 'deny' }));
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
