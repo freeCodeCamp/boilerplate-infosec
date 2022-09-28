f215a5b040c6812c53d2c4180053355f0052790b
Mateus <creztfallen@M.S.M>
2022-09-28 10:33:54 -0300

    finished

--- myApp.js
+++ myApp.js
@@ -2,10 +2,42 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet');
 
+const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
+
 module.exports = app;
 const api = require('./server.js');
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({ action: 'deny' }));
+app.use(
+  helmet({
+    frameguard: {
+      action: 'deny',
+    },
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        styleSrc: ['style.com'],
+      },
+    },
+    dnsPrefetchControl: false,
+  })
+);
+
+// app.use(helmet.hidePoweredBy());
+// app.use(helmet.frameguard({ action: 'deny' }));
+// app.use(helmet.xssFilter());
+// app.use(helmet.noSniff());
+// app.use(helmet.ieNoOpen());
+// app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
+// app.use(helmet.dnsPrefetchControl());
+// app.use(helmet.noCache());
+// app.use(
+//   helmet.contentSecurityPolicy({
+//     directives: {
+//       defaultSrc: ["'self'"],
+//       scriptSrc: ["'self'", 'trusted-cdn.com'],
+//     },
+//   })
+// );
+
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
--- server.js
+++ server.js
@@ -3,43 +3,43 @@
  * the verification process may break
  *******************************************/
 
-var express = require("express");
-var app = express();
-app.disable("x-powered-by");
-var fs = require("fs");
-var path = require("path");
+const express = require('express');
+const app = express();
+app.disable('x-powered-by');
+const fs = require('fs');
+const path = require('path');
 
 app.use(function (req, res, next) {
   res.set({
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers":
-      "Origin, X-Requested-With, content-type, Accept",
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers':
+      'Origin, X-Requested-With, content-type, Accept',
   });
-  app.disable("x-powered-by");
+  app.disable('x-powered-by');
   next();
 });
 
-app.get("/file/*?", function (req, res, next) {
-  if (req.params[0] === ".env") {
-    return next({ status: 401, message: "ACCESS DENIED" });
+app.get('/file/*?', function (req, res, next) {
+  if (req.params[0] === '.env') {
+    return next({ status: 401, message: 'ACCESS DENIED' });
   }
   fs.readFile(path.join(__dirname, req.params[0]), function (err, data) {
     if (err) {
       return next(err);
     }
-    res.type("txt").send(data.toString());
+    res.type('txt').send(data.toString());
   });
 });
 
-var main = require("./myApp.js");
-app.get("/app-info", function (req, res) {
+var main = require('./myApp.js');
+app.get('/app-info', function (req, res) {
   // list middlewares mounted on the '/' camper's app
   var appMainRouteStack = main._router.stack
-    .filter((s) => s.path === "")
+    .filter((s) => s.path === '')
     .map((l) => l.name)
     // filter out express default middlewares
     .filter(
-      (n) => !(n === "query" || n === "expressInit" || n === "serveStatic")
+      (n) => !(n === 'query' || n === 'expressInit' || n === 'serveStatic')
     );
 
   // filter out CORS Headers
@@ -50,19 +50,19 @@ app.get("/app-info", function (req, res) {
   hs.forEach((h) => {
     hObj[h] = res.getHeaders()[h];
   });
-  delete res.get("strict-transport-security");
+  delete res.get('strict-transport-security');
   res.json({ headers: hObj, appStack: appMainRouteStack });
 });
 
-app.get("/package.json", function (req, res, next) {
-  fs.readFile(__dirname + "/package.json", function (err, data) {
+app.get('/package.json', function (req, res, next) {
+  fs.readFile(__dirname + '/package.json', function (err, data) {
     if (err) return next(err);
-    res.type("txt").send(data.toString());
+    res.type('txt').send(data.toString());
   });
 });
 
 app.use(function (req, res, next) {
-  res.status(404).type("txt").send("Not Found");
+  res.status(404).type('txt').send('Not Found');
 });
 
 module.exports = app;
