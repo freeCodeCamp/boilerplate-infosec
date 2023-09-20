d3d2efbf68ada4a13abc03ffb96f26da82ebd948
tboxmy <t_boxmy@yahoo.com>
2023-09-20 17:38:26 +0800

    add helmet

--- myApp.js
+++ myApp.js
@@ -1,61 +1,17 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const helmet = require('helmet')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-let port = process.env.PORT || 3000;
+module.exports = app
+const api = require('./server.js')
+app.use(express.static('public'))
+app.use(helmet())
+app.disable('strict-transport-security')
+app.use('/_api', api)
+app.get('/', function (request, response) {
+  response.sendFile(__dirname + '/views/index.html')
+})
+let port = process.env.PORT || 3000
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
-});
+  console.log(`Your app is listening on port ${port}`)
+})
