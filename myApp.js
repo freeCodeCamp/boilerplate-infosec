ffab1065c211190624a614ddd0a09e540d764dbb
raj01shubham <raj01shubham@gmail.com>
2023-10-31 16:59:56 +0530

    changed case for deny action parameter

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ const helmet = require('helmet');
 const app = express();
 
 app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({action: 'deny'}));
+app.use(helmet.frameguard({action: 'DENY'}));
 
 
 
