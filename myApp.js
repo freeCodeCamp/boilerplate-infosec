89da46a82244d426bd7aee90f01da85beef5ce7b
mtc bamalam <mc_bamalam@protonmail.com>
2024-04-06 09:24:34 +0000

    Clickjacking Submittal

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const app = express();
+app.use(helmet.frameguard({action: 'deny'}));
 const PORT = process.env.PORT || 3000;
 app.use(helmet.hidePoweredBy());
 
