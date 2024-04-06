4d59c60ea9074b9f291dae7c445aaf288f219040
mtc bamalam <mc_bamalam@protonmail.com>
2024-04-06 09:45:42 +0000

    HSTS Force

--- myApp.js
+++ myApp.js
@@ -5,6 +5,8 @@ app.use(helmet.frameguard({action: 'deny'}));
 const PORT = process.env.PORT || 3000;
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
+var ninetyDaysInSeconds = 90*24*60*60;
+app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
 app.use(helmet.xssFilter());
 app.use(helmet.hidePoweredBy());
 
