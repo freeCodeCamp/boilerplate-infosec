061a00772042a438c72943f2a5ae9979105f10e8
mtc bamalam <mc_bamalam@protonmail.com>
2024-04-06 08:55:31 +0000

    origin/main

--- myApp.js
+++ myApp.js
@@ -2,13 +2,13 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(helmet.hidePoweredBy());
 
 module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
-app.use(helmet.hidePoweredBy());
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
